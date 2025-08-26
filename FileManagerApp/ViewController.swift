1f5c171bb500a05a1a10e4768a19f1897e0d95eb
Dmitriy D <akenory@gmail.com>
2025-08-26 15:57:13 +0500

    Initial Commit

---
+++ FileManagerApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FileManagerApp
+//
+//  Created by Дмитрий Дудник on 26.08.2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
